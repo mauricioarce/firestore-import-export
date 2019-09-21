f92e9774b7668e2ba0187768e47b8c071bd46ae4
Mauricio Arce <mauricio.arce.mat@gmail.com>
2019-09-21 12:27:53 -0400

    Configuration for swcbba assistants export

--- export.js
+++ export.js
@@ -1,6 +1,6 @@
-const admin = require("firebase-admin");
+const admin = require('firebase-admin');
 const fs = require('fs');
-const serviceAccount = require("./serviceAccountKey.json");
+const serviceAccount = require('./serviceAccountKey.json');
 
 let collectionName = process.argv[2];
 let subCollection = process.argv[3];
@@ -8,7 +8,7 @@ let subCollection = process.argv[3];
 // You should replace databaseURL with your own
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://ionic-firestore-dn.firebaseio.com"
+  databaseURL: 'https://ionic-firestore-dn.firebaseio.com'
 });
 
 let db = admin.firestore();
@@ -17,47 +17,60 @@ db.settings({ timestampsInSnapshots: true });
 let data = {};
 data[collectionName] = {};
 
-let results = db.collection(collectionName)
-.get()
-.then(snapshot => {
-  snapshot.forEach(doc => {
-    data[collectionName][doc.id] = doc.data();
-  })
-  return data;
-})
-.catch(error => {
-  console.log(error);
-})
+let assistants = [];
 
-results.then(dt => {  
-  getSubCollection(dt).then(() => {    
-    // Write collection to JSON file
-    fs.writeFile("firestore-export.json", JSON.stringify(data), function(err) {
-        if(err) {
-            return console.log(err);
-        }
-        console.log("The file was saved!");
+let results = db
+  .collection(collectionName)
+  .get()
+  .then(snapshot => {
+    snapshot.forEach(doc => {
+      data[collectionName][doc.id] = doc.data();
+      assistants.push(doc.data());
     });
-  })  
-})
+    return assistants;
+  })
+  .catch(error => {
+    console.log(error);
+  });
 
-async function getSubCollection(dt){
-  for (let [key, value] of Object.entries([dt[collectionName]][0])){
-    if(subCollection !== undefined){
+results.then(dt => {
+  fs.writeFile('assistants.json', JSON.stringify(assistants), function(err) {
+    if (err) {
+      return console.log(err);
+    }
+    console.log('The file was saved!');
+  });
+  // getSubCollection(dt).then(() => {
+  //   // Write collection to JSON file
+  //   fs.writeFile('firestore-export.json', JSON.stringify(data), function(err) {
+  //     if (err) {
+  //       return console.log(err);
+  //     }
+  //     console.log('The file was saved!');
+  //   });
+  // });
+});
+
+async function getSubCollection(dt) {
+  for (let [key, value] of Object.entries([dt[collectionName]][0])) {
+    if (subCollection !== undefined) {
       data[collectionName][key]['subCollection'] = {};
-      await addSubCollection(key, data[collectionName][key]['subCollection']);            
-    }          
-  }  
+      await addSubCollection(key, data[collectionName][key]['subCollection']);
+    }
+  }
 }
 
-function addSubCollection(key, subData){
+function addSubCollection(key, subData) {
   return new Promise(resolve => {
-    db.collection(collectionName).doc(key).collection(subCollection).get()
-    .then(snapshot => {      
-      snapshot.forEach(subDoc => {             
-        subData[subDoc.id] =  subDoc.data();
-        resolve('Added data');                                                                 
-      })
-    })
-  })
+    db.collection(collectionName)
+      .doc(key)
+      .collection(subCollection)
+      .get()
+      .then(snapshot => {
+        snapshot.forEach(subDoc => {
+          subData[subDoc.id] = subDoc.data();
+          resolve('Added data');
+        });
+      });
+  });
 }
