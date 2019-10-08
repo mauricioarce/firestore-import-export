e752f902185f4846722ec6b17ac719280039fcc1
Mauricio Arce <mauricio.arce.mat@gmail.com>
2019-10-08 14:27:39 -0400

    Add files

--- export.js
+++ export.js
@@ -2,20 +2,15 @@ const admin = require('firebase-admin');
 const fs = require('fs');
 const serviceAccount = require('./serviceAccountKey.json');
 
-let collectionName = process.argv[2];
-let subCollection = process.argv[3];
+const collectionName = 'assistants';
 
-// You should replace databaseURL with your own
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'https://ionic-firestore-dn.firebaseio.com'
+  databaseURL: 'https://swcbba-wome.firebaseio.com'
 });
 
-let db = admin.firestore();
+const db = admin.firestore();
 db.settings({ timestampsInSnapshots: true });
-
-let data = {};
-data[collectionName] = {};
 
 let assistants = [];
 
@@ -24,8 +19,10 @@ let results = db
   .get()
   .then(snapshot => {
     snapshot.forEach(doc => {
-      data[collectionName][doc.id] = doc.data();
-      assistants.push(doc.data());
+      const assistant = doc.data();
+      if (!assistant.deleteFlag) {
+        assistants.push(doc.data());
+      }
     });
     return assistants;
   })
@@ -40,37 +37,4 @@ results.then(dt => {
     }
     console.log('The file was saved!');
   });
-  // getSubCollection(dt).then(() => {
-  //   // Write collection to JSON file
-  //   fs.writeFile('firestore-export.json', JSON.stringify(data), function(err) {
-  //     if (err) {
-  //       return console.log(err);
-  //     }
-  //     console.log('The file was saved!');
-  //   });
-  // });
 });
-
-async function getSubCollection(dt) {
-  for (let [key, value] of Object.entries([dt[collectionName]][0])) {
-    if (subCollection !== undefined) {
-      data[collectionName][key]['subCollection'] = {};
-      await addSubCollection(key, data[collectionName][key]['subCollection']);
-    }
-  }
-}
-
-function addSubCollection(key, subData) {
-  return new Promise(resolve => {
-    db.collection(collectionName)
-      .doc(key)
-      .collection(subCollection)
-      .get()
-      .then(snapshot => {
-        snapshot.forEach(subDoc => {
-          subData[subDoc.id] = subDoc.data();
-          resolve('Added data');
-        });
-      });
-  });
-}
